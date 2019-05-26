b89eb81e7a7959c2ba75646ad0749fae7894193e
yihyo5400 <yihyo5400@naver.com>
2019-05-27 00:54:01 +0900

    2nd_assignment

--- wordcount/views.py
+++ wordcount/views.py
@@ -11,6 +11,12 @@ def result (request):
     text = request.GET['fulltext']
     words = text.split()
     word_dictionary = {}
+    li = list(text)
+    letter = []
+
+    for i in li:
+        if i != ' ':
+            letter.append(i) #letter에 i 항목을 추가해라
 
     for word in words:
         if word in word_dictionary:
@@ -20,4 +26,4 @@ def result (request):
             #add to dictionary
             word_dictionary[word]=1
 
-    return render (request, 'result.html', {'full': text, 'total': len(words), 'dictionary': word_dictionary.items()})
+    return render (request, 'result.html', {'full': text, 'total': len(words), 'dictionary': word_dictionary.items(), 'letters' : len(letter)})
