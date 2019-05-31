e1c7678e851620ee24c96acd711e805cb30dfb8e
yihyo5400 <yihyo5400@naver.com>
2019-05-31 21:17:06 +0900

    이효영

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!j(-(a8^ou-9-=lyj#@u)=h^2vvzay0flvaqc03-&d3y@f_t=a'
+# SECRET_KEY = '!j(-(a8^ou-9-=lyj#@u)=h^2vvzay0flvaqc03-&d3y@f_t=a'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!j(-(a8^ou-9-=lyj#@u)=h^2vvzay0flvaqc03-&d3y@f_t=a')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True))
 
 ALLOWED_HOSTS = []
 
